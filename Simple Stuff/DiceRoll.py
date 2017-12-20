0f6f22b979ba239ba0dd3b665398be3d2f699551
Michael Knott <34724821+ProbablyKnott@users.noreply.github.com>
2017-12-20 17:28:58 -0500

    Update DiceRoll.py

--- Simple Stuff/DiceRoll.py
+++ Simple Stuff/DiceRoll.py
@@ -1,5 +1,12 @@
 import random
 
+'''
+Written by: Michael Knott
+Date: 11/20/2017
+Github: https://github.com/ProbablyKnott/Simple-Stuff
+Twitter: www.twitter.com/probablyknott
+'''
+
 def main():
 	print("\nThis program outputs a random dice roll.\n")
 
